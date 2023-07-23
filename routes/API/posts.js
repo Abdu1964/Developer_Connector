1b106f6c0a1919dfefdfd56850a296719acac678
unknown <email id>
2023-07-23 17:23:34 +0300

    unlike routes

--- routes/API/posts.js
+++ routes/API/posts.js
@@ -100,4 +100,31 @@ router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req,
         .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
     });
 });
+
+// @route   POST api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          //this means if  they are not there
+          if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+          
+            return res.status(400).json({ notliked: 'You have not yet liked this post' });
+          }
+
+          // Get the remove index
+          const removeIndex = post.likes.map(item => item.user.toString()).indexOf(req.user.id);
+
+          // Splice out of array
+          post.likes.splice(removeIndex, 1);
+
+          // Save the post
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+    });
+});
 module.exports = router;
